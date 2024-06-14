3ecf3e206c7399121e8d64841f6054b9123c53ef
MilkJug <michael.sagan.crawford@gmail.com>
2024-06-14 01:00:40 -0400

    Added more of a story, a warning label, and added more images
    Theres a small bug with one of the options, its due to how I coded it
    unfortunately, did not do a state machine which looking back on would of
    been a good idea.

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ let testText = document.querySelector('.test');
 let bg = document.querySelector('body')
 let image = document.querySelector('.story-image')
 let para = document.querySelector('p')
+let warntext = document.querySelector('.warning')
 
 function changeBtnText(text, index) {
     selBtn(index).innerHTML = text; // changes the value of the 'innerHTML' to what ever we need it to be.
@@ -27,21 +28,32 @@ btn1.addEventListener('click', function() {
         case "Join and Enlist to become a Helldiver": // first ending 
             changeBtnText('Go to Armory', 0);
             changeBtnText('Go to Mission Control', 1);
-            image.src = "https://imgs.search.brave.com/ujjLyopeYiq4Tss0F7X6vADc0MvV_Iy82OaEpi2UtHY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9mYWtl/aW1nLnBsLzYwMHg0/MDA.jpeg"
+            image.src = "src/Ship.png"
             bg.style.background = "" // Change background to ship TODO: Add photo of the ship.
-            para.innerHTML = "You arrive on the ship after your training, you own this ship now. What are you going to name it."
+            para.innerHTML = "You arrive on the ship after your training. In front of you, you see the Armory to your left, and the Mission control center to your left. Which one do you choose?"
+            warntext.style.display = 'block';
             break;
         case "Go to Armory":
+
             para.innerHTML = "You don't have enough samples to buy anything."
             btn2.style.display = "none";
+            image.src = 'src/menu.jpg'
             changeBtnText("Go Back", 0);
-            btn1.addEventListener("click", function() {
-                changeBtnText('Go to Armory', 0)
+            btn1.addEventListener("click", function() { // WARN: USER COULD GO INTO RECURSIVE LOOP
+                changeBtnText('Go to Armory', 0)    // Fix? This whole thing would have to be controlled by states in order to work.
                 changeBtnText('Go to Mission Control', 1);
-                image.src = "https://imgs.search.brave.com/ujjLyopeYiq4Tss0F7X6vADc0MvV_Iy82OaEpi2UtHY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9mYWtl/aW1nLnBsLzYwMHg0/MDA.jpeg"
-                para.innerHTML = "You arrive on the ship after your training, you own this ship now. What are you going to name it."
+                image.src = "src/Ship.png"
+                para.innerHTML = "You arrive on the ship after your training. In front of you, you see the Armory to your left, and the Mission control center to your left. Which one do you choose?"
                 btn2.style.display = "inline-block";
-            })
+            });
+            break;
+        case "Crimsica (Termind Planet)":
+
+
+            para.innerHTML = "You've chosen to go to the bug planet, here you are cleaning out bug holes. During your mission, you see a charger and a Bile Titan. Which one do you take on first?";
+            changeBtnText('Charger', 0);
+            changeBtnText('Bile Titan', 1);
+            image.src = "src/biletitanandcharger.webp"
             break;
 
     }
@@ -49,19 +61,40 @@ btn1.addEventListener('click', function() {
 });
 
 btn2.addEventListener('click', function() {
-    
-    switch(btn2.textContent) {
-        case "Refuse and continue your life as a citizen": 
+
+    switch (btn2.textContent) {
+        case "Refuse and continue your life as a citizen":
             btn1.style.display = "none";
             btn2.style.display = "none";
-
             para.innerHTML = "You died! Refresh the page to go back!"
-            image.src = "https://imgs.search.brave.com/y1tSkzZk_qPAQSQsX1oJPviOrHagSrdCug4c6i2blJc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/c2hvcGlmeS5jb20v/cy9maWxlcy8xLzA1/MzMvMjA4OS9maWxl/cy9wbGFjZWhvbGRl/ci1pbWFnZXMtaW1h/Z2VfbGFyZ2UucG5n/P3Y9MTUzMDEyOTA4/MQ"
+            image.src = "src/death.png"
             break;
         case "Go to Mission Control":
-            image.src //NOTE: MAKE SURE TO UPDATE TEH BACKGROUND WITH PHOTO OF MISSION CONTROL
-            changeBtnText("", 0);
-            changeBtnText("Text", 1) ;
+            changeBtnText("Crimsica (Termind Planet)", 0);
+            changeBtnText("Vernen Wells(Automaton Planet)", 1);
             para.innerHTML = "You chose to go to the Mission Center, a place where you can choose where you want to spread democracy, upon walking up to it, it shows a hologram of 2 planets. ";
+            break;
+
     }
 })
+
+            // btn1.addEventListener('mouseover', function() {
+            //     image.src = 'src/termindplanet.png'
+            // })
+// Code doesn't work correctly
+
+btn1.addEventListener('mouseover', function() {
+    switch (btn1.textContent){
+        case 'Crimsica (Termind Planet)':
+            image.src = 'src/termindplanet.png';
+    }
+})
+
+
+btn2.addEventListener('mouseover', function() {
+    switch (btn2.textContent){
+        case 'Vernen Wells(Automaton Planet)':
+            image.src = 'src/BotPlanet.png';
+    }
+})
+
